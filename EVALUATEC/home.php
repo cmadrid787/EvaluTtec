9e249aee63ced5997947380e8bf90ae2a49f7c66
Carlos Antonio <53875547+cmadrid787@users.noreply.github.com>
2022-12-11 22:15:23 -0600

    Add files via upload

---
+++ EVALUATEC/home.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home</title>
+    <link href="assets/css/style.css" rel="stylesheet">
+</head>
+<body>
+<h1>Bienvenid@ a tus evaluaciones!<h1>
+
+  <video src="assets/video/videoevaluatec.mp4" width="1350" height="650" autoplay muted loop></video>
+
+</body>
+</html>
